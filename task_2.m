de2e02bba37561281167ffcdf71b196332e1f5dd
Cooper Gilberg <gilbergcooper@gmail.com>
2024-02-21 20:54:20 -0700

    Commented Task 2

--- task_2.m
+++ task_2.m
@@ -20,24 +20,25 @@ ts = Ts/lambda;
 original_symbols = [];
 x = [];
 for i = 1:N
-    % Generate M QPSK symbols
+    % Generate M QPSK symbols (Step 1)
     data = randi([0 1], 2*M, 1);
     data_freq = nrSymbolModulate(data, 'QPSK');
     
     original_symbols = [original_symbols; data_freq];
 
-    % Convert to time domain
+    % Convert to time domain (Step 2)
     data_time = ifft(data_freq);
     
+    % Zero padding (Step 3)
     x = [x; zeros(L-1, 1); data_time];
 end
-
+% Ending 0's (Step 3)
 x = [x; zeros(L-1, 1)];
 
-x_zeros = x;
-
+% Upsample (step 4)
 x = upsample(x, lambda);
 
+% Pre-filter Plots
 f = (fs*(-length(x)/2:length(x)/2 - 1)/length(x));
 figure(1)
 plot(f, abs(x));
@@ -51,10 +52,12 @@ hold on
 title("Pre filtered samples spectrogram");
 hold off
 
+% Raised cosine filter (Step 5)
 filter = rcosine(1, lambda, 'sqrt', beta, delay);
 filter = filter.';
 x_filtered = conv(x, filter);
 
+% Post-filter Plots
 f_filtered = (fs*(-length(x_filtered)/2:length(x_filtered)/2 - 1)/length(x_filtered));
 figure(3)
 plot(f_filtered, abs(x_filtered));
@@ -68,12 +71,13 @@ hold on
 title("Filtered samples spectrogram");
 hold off
 
-
+% Convert to pass band (Step 6)
 x_pass_band = zeros(length(x_filtered), 1);
 for i = 0:length(x_filtered)-1
     x_pass_band(i+1) = real(x_filtered(i+1)*exp(j*2*pi*fc*i*ts));
 end
 
+% LFM (Step 7)
 f0 = fc - 4e3;
 f1 = fc + 4e3;
 t1 = .05;
@@ -86,7 +90,7 @@ LFM = [];
 for i = 1:4
    LFM = [LFM; chirp; z]; 
 end
-
+% LFM with OFDM symbols
 lfm_symbols = [LFM; x_pass_band; LFM];
 
 figure(5)
@@ -107,40 +111,27 @@ hold on
 title("OFDM symbols with chirp applied")
 hold off
 
-
-
-% Validation (using LFM)
-% lfm_symbols_base_band_I = zeros(length(lfm_symbols), 1);
-% lfm_symbols_base_band_Q = zeros(length(lfm_symbols), 1);
-% for n = 1: length(lfm_symbols)
-%     lfm_symbols_base_band_I(n) = lfm_symbols(n)*2*cos(2*pi*fc*n*ts); 
-%     lfm_symbols_base_band_Q(n) = -1*lfm_symbols(n)*2*sin(2*pi*fc*n*ts); 
-% end
-% 
-% lfm_symbols_base_band = lfm_symbols_base_band_I + j*lfm_symbols_base_band_Q;
-% 
-% x_ovf = conv(lfm_symbols_base_band, filter);
-% 
-% x_ovf_down_sampled = downsample(x_ovf(2*delay:length(x_ovf)), lambda); 
-% 
-% result = fft(x_ovf_down_sampled); % Go back to frequency domain
-
-
-
 % Validation (using only passband data)
+% Convert to baseband
 lfm_symbols_base_band_I = zeros(length(x_pass_band), 1);
 lfm_symbols_base_band_Q = zeros(length(x_pass_band), 1);
 for n = 0: length(x_pass_band)-1
     lfm_symbols_base_band_I(n+1) = x_pass_band(n+1)*2*cos(2*pi*fc*n*ts); 
     lfm_symbols_base_band_Q(n+1) = -1*x_pass_band(n+1)*2*sin(2*pi*fc*n*ts); 
 end
-
+% Combine I and Q data for complex values
 lfm_symbols_base_band = lfm_symbols_base_band_I + j*lfm_symbols_base_band_Q;
 
+% Pass through filter
 x_ovf = conv(lfm_symbols_base_band, filter);
 
+% Remove delay due to filter
 x_ovf = x_ovf((lambda*delay*2)+1:length(x_ovf));
+% Downsample
 x_ovf_down_sampled = downsample(x_ovf, lambda);
+% Remove leading 0's due to zero padding
 x_ovf_down_sampled = x_ovf_down_sampled(L:length(x_ovf_down_sampled));
+% Remove trailing 0's due to zero padding
 x_ovf_down_sampled = x_ovf_down_sampled(1:M);
-result = fft(x_ovf_down_sampled); % Go back to frequency domain
+% Go back to frequency domain
+result = fft(x_ovf_down_sampled);
