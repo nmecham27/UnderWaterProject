e27ac85f1294abfa653802da921cd0cc8c8afa45
Cooper Gilberg <gilbergcooper@gmail.com>
2024-03-21 21:28:37 -0600

    Completed steps 5-8

--- Part 2/project_part_2.m
+++ Part 2/project_part_2.m
@@ -57,9 +57,55 @@ y_pb_re_192 = upfirdn(y_pb_re, h, Ls, Ms);
 
 [pb_cor,lag] = xcorr(y_pb_re_192, OFDM_data_pre_old);
 
-%pb_cor_length = 1:length(pb_cor);
+pb_cor_length = 1:length(pb_cor);
 figure(2);
 hold on
 title("Correlated passband data");
+% No matter how you plot, peak is 1418947 samples before end of data
 plot(lag, pb_cor); %292723
+%plot(pb_cor_length, pb_cor); % Peak is at 2004390
 hold off
+
+% Want last 1418947 samples
+% y_pb_re_192 is 1711669 samples long
+n0 = length(y_pb_re_192)-1418947;
+y_pb_re_192 = y_pb_re_192(n0:length(y_pb_re_192));
+
+% Step 6 (is this step necessary)
+load("itc_1007_compfilter.mat"); % stores vector in h_comp variable
+y_pb_re_192_conv = conv(y_pb_re_192, h_comp); % adds 50 sample delay
+figure(3);
+hold on
+title("y_pb_re_192_conv");
+plot(1:length(y_pb_re_192_conv), y_pb_re_192_conv);
+hold off
+% Need to remove 50 sample delay
+y_pb_re_192_conv = y_pb_re_192_conv(51:length(y_pb_re_192_conv)); % Not sure if this is correct way to remove delay
+
+% Step 7
+% Convert from passband to baseband
+yBB_I = 1:length(y_pb_re_192_conv);
+yBB_Q = 1:length(y_pb_re_192_conv);
+yBB_I = yBB_I.';
+yBB_Q = yBB_Q.';
+yBB = 1:length(y_pb_re_192_conv);
+yBB = yBB.';
+ts_low = 1/sampling_rate_low;
+for n = 1:length(yBB_I)
+    yBB_I(n) = y_pb_re_192_conv(n)*2*cos(2*pi*Fc*n*ts_low);
+    yBB_Q(n) = -1*y_pb_re_192_conv(n)*2*sin(2*pi*Fc*n*ts_low);
+    yBB(n) = yBB_I(n) + j*yBB_Q(n);
+end
+
+% Step 8
+% Raised cosine filter (arguments come from Part 1 of project)
+lambda = 24;
+beta = .125;
+delay = 100;
+filter = rcosine(1, lambda, 'sqrt', beta, delay);
+filter = filter.';
+yBB_filtered = conv(yBB, filter);
+% Remove delay from filtered data
+yBB_filtered = yBB_filtered((lambda*delay*2)+1:length(yBB_filtered));
+
+
