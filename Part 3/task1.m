386f0c6722f8ea0ee35084da3b0c9e58b3d2957a
Cooper Gilberg <gilbergcooper@gmail.com>
2024-04-15 19:37:39 -0600

    Completed task 2

--- Part 3/task1.m
+++ Part 3/task1.m
@@ -41,7 +41,7 @@ null_index = find(ofdm_map == 0)-1;
 K_n = length(null_index);
 
 % Using the null subcarriers calculate the noise variance
-noise_variance = (1/K_n)*sum(abs(bb_rece_data_172648_1474(null_index+1,:)).^2);
+noise_variance_one = (1/K_n)*sum(abs(bb_rece_data_172648_1474(null_index+1,:)).^2);
 
 % Find the indices of the data subcarriers
 data_index = find(ofdm_map == 2)-1;
@@ -49,17 +49,21 @@ K_d = length(data_index);
 
 % The Zw results but only the data corresponding to
 % data subcarriers
-zd = bb_rece_data_172648_1474(data_index+1,:);
+zd_one = bb_rece_data_172648_1474(data_index+1,:);
 
 % Get the channel estimate corresponding to the data
 % subcarriers
-H_d = H1(data_index+1,:);
+H_d_one = H1(data_index+1,:);
 
 % Calculate the LR
 % The format for the LR matrix is each row corresponds to a
 % differnt OFDM symbol and the colums are L_b1 and L_b1 for
 % each subcarrier
 LR = zeros(2*K_d, W);
+x1 = 1/sqrt(2) + i*1/sqrt(2);
+x2 = -1/sqrt(2) + i*1/sqrt(2);
+x3 = 1/sqrt(2) - i*1/sqrt(2);
+x4 = -1/sqrt(2) - i*1/sqrt(2);
 for ofdm_symbol = 1:W
     for subcarrier = 1:K_d
 %         x_1 = -1*(norm(zd(subcarrier,ofdm_symbol)-H_d(subcarrier,ofdm_symbol)*(1/sqrt(2)+1i*1/sqrt(2)))^2)/noise_variance(ofdm_symbol);
@@ -72,16 +76,11 @@ for ofdm_symbol = 1:W
 % 
 %         LR(ofdm_symbol, (subcarrier*2)-1) = L_b1;
 %         LR(ofdm_symbol, subcarrier*2) = L_b2;
-
-        x1 = 1/sqrt(2) + i*1/sqrt(2);
-        x2 = -1/sqrt(2) + i*1/sqrt(2);
-        x3 = 1/sqrt(2) - i*1/sqrt(2);
-        x4 = -1/sqrt(2) - i*1/sqrt(2);
         
-        A = norm(-1*zd(subcarrier,ofdm_symbol)-H_d(subcarrier,ofdm_symbol).*x1)^2/noise_variance(ofdm_symbol);
-        B = norm(-1*zd(subcarrier,ofdm_symbol)-H_d(subcarrier,ofdm_symbol).*x3)^2/noise_variance(ofdm_symbol);
-        C = norm(-1*zd(subcarrier,ofdm_symbol)-H_d(subcarrier,ofdm_symbol).*x2)^2/noise_variance(ofdm_symbol);
-        D = norm(-1*zd(subcarrier,ofdm_symbol)-H_d(subcarrier,ofdm_symbol).*x4)^2/noise_variance(ofdm_symbol);
+        A = norm(-1*zd_one(subcarrier,ofdm_symbol)-H_d_one(subcarrier,ofdm_symbol).*x1)^2/noise_variance_one(ofdm_symbol);
+        B = norm(-1*zd_one(subcarrier,ofdm_symbol)-H_d_one(subcarrier,ofdm_symbol).*x3)^2/noise_variance_one(ofdm_symbol);
+        C = norm(-1*zd_one(subcarrier,ofdm_symbol)-H_d_one(subcarrier,ofdm_symbol).*x2)^2/noise_variance_one(ofdm_symbol);
+        D = norm(-1*zd_one(subcarrier,ofdm_symbol)-H_d_one(subcarrier,ofdm_symbol).*x4)^2/noise_variance_one(ofdm_symbol);
         
         L_b1 = max(A, B)+log(1+exp(-1*abs(B-A)))-(max(C,D)+log(1+exp(-1*abs(D-C))));
         L_b2 = max(A, C)+log(1+exp(-1*abs(C-A)))-(max(B,D)+log(1+exp(-1*abs(D-B))));
@@ -98,24 +97,24 @@ load("CODE.mat");
 
 NAME = './5G_LDPC_M10_N20_Z142_Q2_nonVer.txt';
 [address, LDPC_INFOLEN] = ldpc_mex_initial_CAPI([1420, 2840, 2], NAME);
-est_code = zeros(2*K_d, W);
-bec = zeros(1, W);
+est_code_one = zeros(2*K_d, W);
+bec_one = zeros(1, W);
 LR_in_de = zeros(length(LR), 1);
 APP_code = zeros(2*K_d, W);
-wec = 0;
+wec_one = 0;
 for ofdm_sym = 1:W
     LR_in_de(INTRLVR) = LR(:,ofdm_sym);
     APP_code(:,ofdm_sym) = ldpcDecoder_CAPI(address, LR_in_de);
-    est_code(:,ofdm_sym) = (APP_code(:,ofdm_sym) < 0);
-    count = sum(abs(est_code(:,ofdm_sym)-CODE(:,ofdm_sym)));
-    bec(ofdm_sym) = count;
+    est_code_one(:,ofdm_sym) = (APP_code(:,ofdm_sym) < 0);
+    count = sum(abs(est_code_one(:,ofdm_sym)-CODE(:,ofdm_sym)));
+    bec_one(ofdm_sym) = count;
     if count ~= 0
-        wec = wec + 1;
+        wec_one = wec_one + 1;
     end
 end
 
-ber = bec/W/length(APP_code);
-bler = wec/W;
+ber_one = bec_one/W/length(APP_code);
+bler_one = wec_one/W;
 
 
 %% Task 2
@@ -143,4 +142,138 @@ h_ls_three = (1/K_p)*V'*D'*zp3;
 H3 = freq_v*h_ls_three;
 
 %% Part b
+% Get the channel estimate corresponding to the data
+% subcarriers of second hydrophone
+H_d_two = H2(data_index+1,:);
 
+% Calculate norm(Hk) using only subcarriers data was sent
+norm_Hk_2_hydro = zeros(1,W);
+for ofdm_sym = 1:W
+   norm_Hk_2_hydro(ofdm_sym) = sqrt(sum(H_d_one(:, ofdm_sym)'*H_d_one(:, ofdm_sym))+sum(H_d_two(:, ofdm_sym)'*H_d_two(:, ofdm_sym)));
+end
+
+% Received data on hydrophone 2
+zd_two = bb_rece_data_172648_1475(data_index+1,:);
+% Variance on hydrophone 2
+noise_variance_two = (1/K_n)*sum(abs(bb_rece_data_172648_1475(null_index+1,:)).^2);
+
+% Calculate variance when combining hydrophones 1 and 2
+noise_variance_2_hydro = zeros(1, W);
+for ofdm_sym = 1: W
+    noise_variance_2_hydro(ofdm_sym) = sum(H_d_one(:, ofdm_sym)'*H_d_one(:, ofdm_sym))*noise_variance_one(ofdm_sym)+sum(H_d_two(:, ofdm_sym)'*H_d_two(:, ofdm_sym))*noise_variance_two(ofdm_sym);
+end
+noise_variance_2_hydro = noise_variance_2_hydro.*(1./norm_Hk_2_hydro.^2);
+
+% Calculate qk (scalar)
+qk_2_hydro = zeros(K_d, W);
+for ofdm_sym = 1: W
+   for subc = 1:K_d
+      qk_2_hydro(subc, ofdm_sym) = ([H_d_one(subc, ofdm_sym);H_d_two(subc, ofdm_sym)]'*[zd_one(subc, ofdm_sym);zd_two(subc, ofdm_sym)])/norm_Hk_2_hydro(ofdm_sym); 
+   end
+end
+
+% Calculate LLR
+LR_2_hydro = zeros(2*K_d, W);
+for ofdm_sym = 1:W
+    for subc = 1:K_d
+        A = (-1*abs(qk_2_hydro(subc, ofdm_sym)-norm_Hk_2_hydro(ofdm_sym)*x1)^2)/noise_variance_2_hydro(ofdm_sym);
+        B = (-1*abs(qk_2_hydro(subc, ofdm_sym)-norm_Hk_2_hydro(ofdm_sym)*x3)^2)/noise_variance_2_hydro(ofdm_sym);
+        C = (-1*abs(qk_2_hydro(subc, ofdm_sym)-norm_Hk_2_hydro(ofdm_sym)*x2)^2)/noise_variance_2_hydro(ofdm_sym);
+        D = (-1*abs(qk_2_hydro(subc, ofdm_sym)-norm_Hk_2_hydro(ofdm_sym)*x4)^2)/noise_variance_2_hydro(ofdm_sym);
+        
+        L_b1 = max(A, B)+log(1+exp(-1*abs(B-A)))-(max(C,D)+log(1+exp(-1*abs(D-C))));
+        L_b2 = max(A, C)+log(1+exp(-1*abs(C-A)))-(max(B,D)+log(1+exp(-1*abs(D-B))));
+        
+        LR_2_hydro((subc*2)-1, ofdm_sym) = L_b1;
+        LR_2_hydro(subc*2, ofdm_sym) = L_b2;
+    end
+end
+
+%% Part c
+% Calculate ber and bler with 2 hydrophones
+est_code_two = zeros(2*K_d, W);
+bec_two = zeros(1, W);
+LR_in_de = zeros(length(LR_2_hydro), 1);
+APP_code = zeros(2*K_d, W);
+wec_two = 0;
+for ofdm_sym = 1:W
+    LR_in_de(INTRLVR) = LR_2_hydro(:,ofdm_sym);
+    APP_code(:,ofdm_sym) = ldpcDecoder_CAPI(address, LR_in_de);
+    est_code_two(:,ofdm_sym) = (APP_code(:,ofdm_sym) < 0);
+    count = sum(abs(est_code_two(:,ofdm_sym)-CODE(:,ofdm_sym)));
+    bec_two(ofdm_sym) = count;
+    if count ~= 0
+        wec_two = wec_two + 1;
+    end
+end
+
+ber_two = bec_two/W/length(APP_code);
+bler_two = wec_two/W;
+
+%% Part d
+% Get the channel estimate corresponding to the data
+% subcarriers of third hydrophone
+H_d_three = H3(data_index+1,:);
+
+% Calculate norm(Hk) using only subcarriers data was sent
+norm_Hk_3_hydro = zeros(1,W);
+for ofdm_sym = 1:W
+   norm_Hk_3_hydro(ofdm_sym) = sqrt(sum(H_d_one(:, ofdm_sym)'*H_d_one(:, ofdm_sym))+sum(H_d_two(:, ofdm_sym)'*H_d_two(:, ofdm_sym))+sum(H_d_three(:, ofdm_sym)'*H_d_three(:, ofdm_sym)));
+end
+
+% Received data on hydrophone 3
+zd_three = bb_rece_data_172648_1476(data_index+1,:);
+% Variance on hydrophone 3
+noise_variance_three = (1/K_n)*sum(abs(bb_rece_data_172648_1476(null_index+1,:)).^2);
+
+% Calculate variance when combining hydrophones 1, 2, and 3
+noise_variance_3_hydro = zeros(1, W);
+for ofdm_sym = 1: W
+    noise_variance_3_hydro(ofdm_sym) = sum(H_d_one(:, ofdm_sym)'*H_d_one(:, ofdm_sym))*noise_variance_one(ofdm_sym)+sum(H_d_two(:, ofdm_sym)'*H_d_two(:, ofdm_sym))*noise_variance_two(ofdm_sym)+sum(H_d_three(:, ofdm_sym)'*H_d_three(:, ofdm_sym))*noise_variance_three(ofdm_sym);
+end
+noise_variance_3_hydro = noise_variance_3_hydro.*(1./norm_Hk_3_hydro.^2);
+
+% Calculate qk (scalar)
+qk_3_hydro = zeros(K_d, W);
+for ofdm_sym = 1: W
+   for subc = 1:K_d
+      qk_3_hydro(subc, ofdm_sym) = ([H_d_one(subc, ofdm_sym);H_d_two(subc, ofdm_sym);H_d_three(subc, ofdm_sym)]'*[zd_one(subc, ofdm_sym);zd_two(subc, ofdm_sym);zd_three(subc, ofdm_sym)])/norm_Hk_3_hydro(ofdm_sym); 
+   end
+end
+
+% Calculate LLR
+LR_3_hydro = zeros(2*K_d, W);
+for ofdm_sym = 1:W
+    for subc = 1:K_d
+        A = (-1*abs(qk_3_hydro(subc, ofdm_sym)-norm_Hk_3_hydro(ofdm_sym)*x1)^2)/noise_variance_3_hydro(ofdm_sym);
+        B = (-1*abs(qk_3_hydro(subc, ofdm_sym)-norm_Hk_3_hydro(ofdm_sym)*x3)^2)/noise_variance_3_hydro(ofdm_sym);
+        C = (-1*abs(qk_3_hydro(subc, ofdm_sym)-norm_Hk_3_hydro(ofdm_sym)*x2)^2)/noise_variance_3_hydro(ofdm_sym);
+        D = (-1*abs(qk_3_hydro(subc, ofdm_sym)-norm_Hk_3_hydro(ofdm_sym)*x4)^2)/noise_variance_3_hydro(ofdm_sym);
+        
+        L_b1 = max(A, B)+log(1+exp(-1*abs(B-A)))-(max(C,D)+log(1+exp(-1*abs(D-C))));
+        L_b2 = max(A, C)+log(1+exp(-1*abs(C-A)))-(max(B,D)+log(1+exp(-1*abs(D-B))));
+        
+        LR_3_hydro((subc*2)-1, ofdm_sym) = L_b1;
+        LR_3_hydro(subc*2, ofdm_sym) = L_b2;
+    end
+end
+
+% Calculate ber and bler with 3 hydrophones
+est_code_three = zeros(2*K_d, W);
+bec_three = zeros(1, W);
+LR_in_de = zeros(length(LR_3_hydro), 1);
+APP_code = zeros(2*K_d, W);
+wec_three = 0;
+for ofdm_sym = 1:W
+    LR_in_de(INTRLVR) = LR_3_hydro(:,ofdm_sym);
+    APP_code(:,ofdm_sym) = ldpcDecoder_CAPI(address, LR_in_de);
+    est_code_three(:,ofdm_sym) = (APP_code(:,ofdm_sym) < 0);
+    count = sum(abs(est_code_three(:,ofdm_sym)-CODE(:,ofdm_sym)));
+    bec_three(ofdm_sym) = count;
+    if count ~= 0
+        wec_three = wec_three + 1;
+    end
+end
+
+ber_three = bec_three/W/length(APP_code);
+bler_three = wec_three/W;
